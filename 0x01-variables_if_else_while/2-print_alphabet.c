46e69def5e4c0626a85f4e47d5a448e6468af6e3
chichie63 <tyleemoyo@gmail.com>
2023-07-18 06:52:56 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Printing the alphabet in lowercase
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
