2367bf7579429e7425277087cc00dc15092e9191
chichie63 <tyleemoyo@gmail.com>
2023-07-24 06:27:10 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 48; pr <= 57; pr++)
+	{
+		_putchar(pr);
+	}
+	_putchar('\n');
+}
