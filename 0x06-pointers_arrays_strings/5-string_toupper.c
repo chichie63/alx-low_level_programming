c849b47350fc2d6ffbc67120d4e50a82f2d2d253
chichie63 <tyleemoyo@gmail.com>
2023-08-13 14:36:56 -0700

    ./5-string_toupper , git commit -m ./5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,10 +8,12 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	i = 0;
-	while (n[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
+	{
 		if (n[i] >= 'a' && n[i] <= 'z')
+		{
 			n[i] = n[i] - 32;
-	i++;
+		}
+	}
 	return (n);
 }
