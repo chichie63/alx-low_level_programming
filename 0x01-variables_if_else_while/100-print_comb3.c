ff538f33f44c794c3ff54a16f6bd8bfd7915f469
chichie63 <tyleemoyo@gmail.com>
2023-08-29 06:57:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,26 @@
 int main(void)
 {
 	int a = '0';
-
-	for (a = '0'; a <= '9'; a++)
-	{
 	int b = '0';
 
 	for (b = '0'; b <= '9'; b++)
 	{
-		if (!((a == b) || (b > a)))
+		for (a = '0'; a <= '9'; a++)
 		{
-			putchar(b);
-			putchar(a);
-			if (!(a == '9' && b == '8'))
+			if (!((a == b) || (b > a)))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(b);
+				putchar(a);
+				if (!(a == '9' && b == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-	}
 
 	putchar('\n');
+
 	return (0);
 }
