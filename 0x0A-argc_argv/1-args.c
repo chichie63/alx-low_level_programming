03422a8db1e5fc57caf445d863d4af546a8f9b97
chichie63 <tyleemoye@gmail.com>
2024-02-14 11:38:48 -0800

    numbers

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints a number
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
